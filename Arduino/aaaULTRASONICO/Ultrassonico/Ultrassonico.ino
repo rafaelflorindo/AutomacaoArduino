dbf1a8e339eeb78cf3601a73b92dfcb5d4a5f824
rafaelflorindo <rafaelflorindo@hotmail.com>
2018-10-23 16:59:38 -0300

    Add files via upload

---
+++ Arduino/aaaULTRASONICO/Ultrassonico/Ultrassonico.ino
@@ -0,0 +1,21 @@
+#include <Ultrasonic.h>
+ 
+#define TRIGGER_PIN 10
+#define ECHO_PIN 11
+ 
+Ultrasonic ultrasonic(TRIGGER_PIN, ECHO_PIN);
+float dist, metro;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  long microsec = ultrasonic.timing();
+  dist = ultrasonic.convert(microsec, Ultrasonic::CM);
+  metro = dist/100;
+  Serial.print(metro);
+  Serial.print(" metros.");
+  Serial.println();
+  delay(2000);
+}
